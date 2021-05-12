d30cfe2c5c0efc5ea137a6c2af8e58fd9e40fc11
WagnerTerry <wagner.goncalves@calindra.com.br>
2021-05-12 19:26:55 -0300

    aula 02 - Navegacao e UX

--- README.md
+++ README.md
@@ -3,6 +3,13 @@ NLW 5 TRILHA REACT NATIVE
 - expo init plantmanager
 - expo start // inicia o projeto
 
+- expo install @expo/vector-icons
+- expo install expo-font @expo-google-fonts/jost
+- expo install expo-app-loading
+- yarn add @react-navigation/native
+- expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+- yarn add @react-navigation/stack
+
 Criar um arquivo na raiz ( custom.d.ts), para importar imagem com ts
 
 dentro do arquivo custom.d.ts
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,41 +1,50 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   SafeAreaView,
   Text,
-  View,
   Image,
   TouchableOpacity,
   StyleSheet,
-  Touchable,
+  Dimensions,
+  View,
 } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 import wateringImg from "../assets/watering.png";
 import colors from "../styles/colors";
-import { Button } from "../components/Button";
+import fonts from "../styles/fonts";
+import { useNavigation } from "@react-navigation/core";
 
 export function Welcome() {
-  const [visible, setVisible] = useState(false);
+  const navigation = useNavigation();
 
-  function handleVisibility() {
-    setVisible(true);
+  function handleStart() {
+    navigation.navigate("UserIdentification");
   }
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {"\n"}
-        suas plantas{"\n"}
-        de forma fácil
-      </Text>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {"\n"}
+          suas plantas de{"\n"}
+          forma fácil
+        </Text>
 
-      {visible && <Image source={wateringImg} style={styles.image} />}
+        <Image source={wateringImg} style={styles.image} resizeMode="contain" />
 
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrarvocê
-        sempre que precisar.
-      </Text>
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas. Nós cuidamos de lembrarvocê
+          sempre que precisar.
+        </Text>
 
-      <Button title=">" onPress={handleVisibility} />
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.7}
+          onPress={handleStart}
+        >
+          <Feather name="chevron-right" style={styles.buttonIcon} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -43,21 +52,28 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "space-between",
+  },
+  wrapper: {
+    flex: 1,
+    justifyContent: "space-around",
     alignItems: "center",
+    paddingHorizontal: 20,
   },
   title: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: "bold",
     textAlign: "center",
     color: colors.heading,
     marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 34,
   },
   subtitle: {
     textAlign: "center",
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading,
+    fontFamily: fonts.text,
   },
   button: {
     backgroundColor: colors.green,
@@ -66,15 +82,13 @@ const styles = StyleSheet.create({
     borderRadius: 16,
     marginBottom: 10,
     height: 56,
-    // width: 56,
-    paddingHorizontal: 10,
+    width: 56,
   },
   image: {
-    width: 292,
-    height: 284,
+    height: Dimensions.get("window").width * 0.7,
   },
-  buttonText: {
+  buttonIcon: {
+    fontSize: 32,
     color: colors.white,
-    fontSize: 24,
   },
 });
